52648c98aebf7117bee5155ba680b03640b12b49
Nikos Alexandris <nik@nikosalexandris.net>
2015-05-10 11:56:47 +0300

    Independent test for landsat8_mtl.py

--- landsat8_mtl.py
+++ landsat8_mtl.py
@@ -23,6 +23,13 @@ def set_mtlfile():
         return False
 
 
+# where to use this?
+def get_float_from_mtl_line(string):
+    import re
+    return float(re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?",
+                 string)[-1])
+
+
 class Landsat8_MTL():
     """
     Retrieve metadata from a Landsat8's MTL file.
@@ -32,17 +39,25 @@ class Landsat8_MTL():
         """
         Initialise class object based on a Landsat8 MTL filename.
         """
-        import os
-
         # read lines
         with open(mtl_filename, 'r') as mtl_file:
                 mtl_lines = mtl_file.readlines()
+
+        # close and remove 'mtl_file'
         mtl_file.close()
         del(mtl_file)
 
         # clean and convert MTL lines in to a named tuple
-        self.mtl = self.list_to_namedtuple(mtl_lines, 'metadata')
- 
+        self.mtl = self._to_namedtuple(mtl_lines, 'metadata')
+
+        # is it possible to feed directly to self?
+        #for field in self.mtl._fields:
+        #    field_lowercase = field.lower()
+        #    print "Field of named tuple:", field_lowercase 
+        #    self.test.field_lowercase = self.mtl.'{f}'.format(f=field)
+        #    print self.test.field_lowercase
+        #print
+        
         # basic metadata
         self.scene_id = self.mtl.LANDSAT_SCENE_ID
         self.wrs_path = self.mtl.WRS_PATH
@@ -53,12 +68,28 @@ class Landsat8_MTL():
         self.corner_lr = (self.mtl.CORNER_LR_LAT_PRODUCT, self.mtl.CORNER_LR_LON_PRODUCT)
         self.corner_ul_projection = (self.mtl.CORNER_UL_PROJECTION_X_PRODUCT, self.mtl.CORNER_UL_PROJECTION_Y_PRODUCT)
         self.corner_lr_projection = (self.mtl.CORNER_LR_PROJECTION_X_PRODUCT, self.mtl.CORNER_LR_PROJECTION_Y_PRODUCT)
+        self.cloud_cover = self.mtl.CLOUD_COVER
+        self.sun_azimuth = self.mtl.SUN_AZIMUTH
+        self.sun_elevation = self.mtl.SUN_ELEVATION
+        self.earth_sun_distance = self.mtl.EARTH_SUN_DISTANCE
+        self.map_projection = self.mtl.MAP_PROJECTION
+        self.datum = self.mtl.DATUM
+        self.ellipsoid = self.mtl.ELLIPSOID
+        self.utm_zone = self.mtl.UTM_ZONE
+        self.grid_cell_size_panchromatic = self.mtl.GRID_CELL_SIZE_PANCHROMATIC
+        self.grid_cell_size_reflective = self.mtl.GRID_CELL_SIZE_REFLECTIVE
+        self.grid_cell_size_thermal = self.mtl.GRID_CELL_SIZE_THERMAL
 
         # get band filenames
-        #self.band_filenames = [line.strip() for line in self._mtl_lines if 'FILE_NAME_BAND' in line]
+        # self.band_filenames = [line.strip() for line in self._mtl_lines if 'FILE_NAME_BAND' in line]
     
-    def list_to_namedtuple(self, list_of_lines, name_for_tuple):
+    def _to_namedtuple(self, list_of_lines, name_for_tuple):
         """
+        This function performs the following actions on the given
+        'list_of_lines':
+        - exclude lines containing the strings 'GROUP' and 'END'
+        - remove whitespaces and doublequotes from strings
+        - convert list of lines in to a named tuple
         """
         import string
         
@@ -75,14 +106,13 @@ class Landsat8_MTL():
         # loop over lines, do some cleaning
         field_names = []
         field_values = []
-
         for idx in range(len(lines)):
 
             # split line in '='
             line = lines[idx]
             line_split = line.split('=')
 
-            # get name, value and clean whitespaces, '"'
+            # get field name & field value, clean whitespaces and "
             field_name = line_split[0].strip()
             field_names.append(field_name)
             field_value = line_split[1].strip()
@@ -92,19 +122,22 @@ class Landsat8_MTL():
         # named tuple
         named_tuple = namedtuple(name_for_tuple, field_names)
 
+        # return named tuple
         return named_tuple(*field_values)
 
     def __str__(self):
         """
+        Return a string representation of the scene's id.
         """
         msg = 'Landsat8 scene ID:'
         return msg + ' ' + self.scene_id
 
     def _get_mtl_lines(self):
         """
+        Return the "hidden" copy of the MTL lines before cleaning (lines
+        containing 'GROUP' or 'END' are though excluded).
         """
         return self._mtl_lines
-
 
 
 def main():
@@ -117,35 +150,6 @@ def main():
     else:
         MTLFILE = ''
     
-def test(mtlfile):
-    """
-    Code and test...
-    """
-
-    if not mtlfile:
-        print "! No file defined, testing with default MTl file!"
-        mtl = Landsat8_MTL("/geo/grassdb/ellas/meteora/LC81840332014146LGN00/cell_misc/LC81840332014146LGN00_MTL.txt")
-        print
-        
-    else:
-        mtl = Landsat8_MTL(mtlfile)
-
-    print "| MTL object:", mtl
-    print "| Test method _get_mtl_lines:\n ", mtl._get_mtl_lines()
-    print
-
-    print "| Basic metadata:\n"
-    print mtl.scene_id
-    print mtl.wrs_path
-    print mtl.wrs_row
-    print mtl.date_acquired
-    print mtl.scene_center_time
-    print mtl.corner_ul
-    print mtl.corner_lr
-    print mtl.corner_ul_projection
-    print mtl.corner_lr_projection
-
-#test(MTLFILE)
 
 if __name__ == "__main__":
     main()
