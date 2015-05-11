682e989ca657eeefe395d71d74a870446e3d0d45
Nikos Alexandris <nik@nikosalexandris.net>
2015-05-11 18:17:03 +0300

    Set all MTL fields as attributes to self automatically\!

--- landsat8_mtl.py
+++ landsat8_mtl.py
@@ -53,40 +53,22 @@ class Landsat8_MTL():
 
         # clean and convert MTL lines in to a named tuple
         self.mtl = self._to_namedtuple(mtl_lines, 'metadata')
+        self._set_attributes()
 
-        # is it possible to feed directly to self?
-        #for field in self.mtl._fields:
-        #    field_lowercase = field.lower()
-        #    print "Field of named tuple:", field_lowercase 
-        #    self.test.field_lowercase = self.mtl.'{f}'.format(f=field)
-        #    print self.test.field_lowercase
-        #print
-        
-        # basic metadata
+        # shorten LANDSAT_SCENE_ID
         self.scene_id = self.mtl.LANDSAT_SCENE_ID
-        self.wrs_path = self.mtl.WRS_PATH
-        self.wrs_row = self.mtl.WRS_ROW
-        self.date_acquired = self.mtl.DATE_ACQUIRED
-        self.scene_center_time = self.mtl.SCENE_CENTER_TIME
-        self.corner_ul = (self.mtl.CORNER_UL_LAT_PRODUCT, self.mtl.CORNER_UL_LON_PRODUCT)
-        self.corner_lr = (self.mtl.CORNER_LR_LAT_PRODUCT, self.mtl.CORNER_LR_LON_PRODUCT)
-        self.corner_ul_projection = (self.mtl.CORNER_UL_PROJECTION_X_PRODUCT, self.mtl.CORNER_UL_PROJECTION_Y_PRODUCT)
-        self.corner_lr_projection = (self.mtl.CORNER_LR_PROJECTION_X_PRODUCT, self.mtl.CORNER_LR_PROJECTION_Y_PRODUCT)
+       
+        # bounding box related
+        self.corner_ul = (self.mtl.CORNER_UL_LAT_PRODUCT,
+                          self.mtl.CORNER_UL_LON_PRODUCT)
+        self.corner_lr = (self.mtl.CORNER_LR_LAT_PRODUCT,
+                          self.mtl.CORNER_LR_LON_PRODUCT)
+        self.corner_ul_projection = (self.mtl.CORNER_UL_PROJECTION_X_PRODUCT,
+                                     self.mtl.CORNER_UL_PROJECTION_Y_PRODUCT)
+        self.corner_lr_projection = (self.mtl.CORNER_LR_PROJECTION_X_PRODUCT,
+                                     self.mtl.CORNER_LR_PROJECTION_Y_PRODUCT)
         self.cloud_cover = self.mtl.CLOUD_COVER
-        self.sun_azimuth = self.mtl.SUN_AZIMUTH
-        self.sun_elevation = self.mtl.SUN_ELEVATION
-        self.earth_sun_distance = self.mtl.EARTH_SUN_DISTANCE
-        self.map_projection = self.mtl.MAP_PROJECTION
-        self.datum = self.mtl.DATUM
-        self.ellipsoid = self.mtl.ELLIPSOID
-        self.utm_zone = self.mtl.UTM_ZONE
-        self.grid_cell_size_panchromatic = self.mtl.GRID_CELL_SIZE_PANCHROMATIC
-        self.grid_cell_size_reflective = self.mtl.GRID_CELL_SIZE_REFLECTIVE
-        self.grid_cell_size_thermal = self.mtl.GRID_CELL_SIZE_THERMAL
 
-        # get band filenames
-        # self.band_filenames = [line.strip() for line in self._mtl_lines if 'FILE_NAME_BAND' in line]
-    
     def _to_namedtuple(self, list_of_lines, name_for_tuple):
         """
         This function performs the following actions on the given
@@ -104,10 +86,7 @@ class Landsat8_MTL():
         self._mtl_lines = lines
         del(list_of_lines)
 
-        # list comprehension below not easy to read!
-        # self.mtl_lines = [(self.mtl_lines[idx].split('=')[0].strip(), self.mtl_lines[idx].split('=')[1].strip().translate(string.maketrans("", "", ), '"')) for idx in range(len(self.mtl_lines))]
-
-        #
+        # empty variables to hold values
         field_names = []
         field_values = []
         dictionary = {}
@@ -134,20 +113,19 @@ class Landsat8_MTL():
         # named tuple
         named_tuple = namedtuple(name_for_tuple, field_names)
 
-        ### testing below... remove!
-        # query = 'RADIANCE_MULT_BAND_'
-        # band = '10'
-        # requested_field = query + band
-        # some_value = getattr(named_tuple, requested_field)
-        # print "Some example:", some_value
-        ### testing above... remove!
-
-#        print "Named tuple as a dictionary:", named_tuple._asdict()
-        print
-
         # return named tuple
         return named_tuple(*field_values)
 
+    def _set_attributes(self):
+        """
+        Set all parsed field names and values, from the MTL file, fed to the
+        named tuple 'self.mtl', as attributes to the object.
+        """
+        for field in self.mtl._fields:
+            field_lowercase = field.lower()
+            field_value = getattr(self.mtl, field)
+            setattr(self, field_lowercase, field_value)
+    
     def __str__(self):
         """
         Return a string representation of the scene's id.
