9246b2962cb501047066f47ef64e4419cfdec9fe
Nikos Alexandris <nik@nikosalexandris.net>
2020-06-19 16:28:04 +0200

    Move temperature estimation related functions to temperature.py

--- i.landsat8.swlst.py
+++ i.landsat8.swlst.py
@@ -354,55 +354,12 @@ from constants import DUMMY_Rji
 from constants import EQUATION as equation
 from radiance import digital_numbers_to_radiance
 from radiance import radiance_to_brightness_temperature
+from temperature import tirs_to_at_satellite_temperature
+from temperature import estimate_lst
 
 if "GISBASE" not in os.environ:
     print("You must be in GRASS GIS to run this program.")
     sys.exit(1)
-
-def tirs_to_at_satellite_temperature(tirs_1x, mtl_file):
-    """
-    Helper function to convert TIRS bands 10 or 11 in to at-satellite
-    temperatures.
-
-    This function uses the pre-defined functions:
-
-    - extract_number_from_string()
-    - digital_numbers_to_radiance()
-    - radiance_to_brightness_temperature()
-
-    The inputs are:
-
-    - a name for the input tirs band (10 or 11)
-    - a Landsat8 MTL file
-
-    The output is a temporary at-Satellite Temperature map.
-    """
-    # which band number and MTL file
-    band_number = extract_number_from_string(tirs_1x)
-    tmp_radiance = tmp_map_name('radiance') + '.' + band_number
-    tmp_brightness_temperature = tmp_map_name('brightness_temperature') + '.' + \
-        band_number
-    landsat8 = Landsat8_MTL(mtl_file)
-
-    # rescale DNs to spectral radiance
-    radiance_expression = landsat8.toar_radiance(band_number)
-    digital_numbers_to_radiance(tmp_radiance, tirs_1x, radiance_expression)
-
-    # convert spectral radiance to at-satellite temperature
-    temperature_expression = landsat8.radiance_to_temperature(band_number)
-    radiance_to_brightness_temperature(tmp_brightness_temperature,
-                                       tmp_radiance,
-                                       temperature_expression)
-
-
-    # save Brightness Temperature map?
-    if brightness_temperature_prefix:
-        bt_output = brightness_temperature_prefix + band_number
-        run('g.rename', raster=(tmp_brightness_temperature, bt_output))
-        tmp_brightness_temperature = bt_output
-
-    return tmp_brightness_temperature
-
 
 def replace_dummies(string, *args, **kwargs):
     """
@@ -601,66 +558,6 @@ def estimate_cwv_big_expression(outname, t10, t11, cwv_expression):
             history=history_cwv)
 
         run('g.rename', raster=(outname, cwv_output))
-
-
-def estimate_lst(outname, t10, t11, avg_lse_map, delta_lse_map, cwv_map, lst_expression):
-    """
-    Produce a Land Surface Temperature map based on a mapcalc expression
-    returned from a SplitWindowLST object.
-
-    Inputs are:
-
-    - brightness temperature maps t10, t11
-    - column water vapor map
-    - a temporary filename
-    - a valid mapcalc expression
-    """
-    msg = '\n|i Estimating land surface temperature '
-    if info:
-        msg += "| Expression:\n"
-    g.message(msg)
-
-    if info:
-        msg = lst_expression
-        msg += '\n'
-        g.message(msg)
-
-    if landcover_map:
-        split_window_expression = replace_dummies(lst_expression,
-                                                  in_avg_lse=DUMMY_MAPCALC_STRING_AVG_LSE,
-                                                  out_avg_lse=avg_lse_map,
-                                                  in_delta_lse=DUMMY_MAPCALC_STRING_DELTA_LSE,
-                                                  out_delta_lse=delta_lse_map,
-                                                  in_cwv=DUMMY_MAPCALC_STRING_CWV,
-                                                  out_cwv=cwv_map,
-                                                  in_ti=DUMMY_MAPCALC_STRING_T10,
-                                                  out_ti=t10,
-                                                  in_tj=DUMMY_MAPCALC_STRING_T11,
-                                                  out_tj=t11)
-    elif emissivity_class:
-        split_window_expression = replace_dummies(lst_expression,
-                                                  in_cwv=DUMMY_MAPCALC_STRING_CWV,
-                                                  out_cwv=cwv_map,
-                                                  in_ti=DUMMY_MAPCALC_STRING_T10,
-                                                  out_ti=t10,
-                                                  in_tj=DUMMY_MAPCALC_STRING_T11,
-                                                  out_tj=t11)
-
-    if rounding:
-        split_window_expression = '(round({swe}, 2, 0.5))'.format(swe=split_window_expression)
-
-    if celsius:
-        split_window_expression = '({swe}) - 273.15'.format(swe=split_window_expression)
-
-
-    # else:
-    split_window_equation = equation.format(result=outname,
-            expression=split_window_expression)
-
-    grass.mapcalc(split_window_equation, overwrite=True)
-
-    if info:
-        run('r.info', map=outname, flags='r')
 
 
 def main():
